db7bb056f4c6d968be405409ab3612fe1715e010
Jiace Sun <susyustc@gmail.com>
2026-07-16 17:26:55 -0700

    add vxc

--- do_vxc.py
+++ do_vxc.py
@@ -1,12 +1,14 @@
 import os
 import pickle
 import argparse
+
+import h5py
 import numpy as np
-from fcdmft.gw.pbc.krgw_ac import KRGWAC
 import pyscf
+from functools import reduce
+
 import system_common
-import threadpoolctl
-threadpoolctl.threadpool_limits(1)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("system")
@@ -27,28 +29,27 @@ data_dir = system_common.get_data_dir(system, basis, suffix=suffix)
 dft_pkl = os.path.join(data_dir, f"DFT_{klabel}.pkl")
 gdf_chk = os.path.join(data_dir, f"GDF_{klabel}.chk")
 gw_cache_dir = os.path.join(data_dir, f"GWcache_{klabel}")
+vxc_path = os.path.join(gw_cache_dir, "vxc.h5")
+
 with open(dft_pkl, "rb") as f:
     mf = pickle.load(f)
     mf.with_df._cderi = gdf_chk
     mf.max_memory = pyscf.lib.parameters.MAX_MEMORY
 
-gw_energy_path = os.path.join(data_dir, f"GWenergy_{klabel}.npy")
-gw_pkl_path = os.path.join(data_dir, f"GW_{klabel}.pkl")
+nkpts = len(mf.kpts)
+nmo = mf.mo_coeff[0].shape[1]
+mo_coeff = np.asarray(mf.mo_coeff)
 
-fc = system_common.load_section_setting(system, basis, "gw", "fc", suffix=suffix, default=True)
-gw = KRGWAC(mf)
-gw.fc = fc
-gw.verbose = 5
-gw.writefile = 1
-gw.load_vxc = True
+with pyscf.lib.temporary_env(mf, verbose=0), pyscf.lib.temporary_env(mf.mol, verbose=0), pyscf.lib.temporary_env(mf.with_df, verbose=0):
+    dm = mf.make_rdm1()
+    v_mf_ao = mf.get_veff() - mf.get_j(dm_kpts=dm)
+
+v_mf = np.zeros((nkpts, nmo, nmo), dtype=np.complex128)
+for k in range(nkpts):
+    v_mf[k] = reduce(np.matmul, (mo_coeff[k].T.conj(), v_mf_ao[k], mo_coeff[k]))
+
 os.makedirs(gw_cache_dir, exist_ok=True)
-cwd = os.getcwd()
-os.chdir(gw_cache_dir)
-gw.kernel()
-os.chdir(cwd)
+with h5py.File(vxc_path, "w") as f:
+    f["v_mf"] = np.asarray(v_mf)
 
-from mpi4py import MPI
-rank = MPI.COMM_WORLD.Get_rank()
-if rank == 0:
-    np.save(gw_energy_path, gw.mo_energy)
-    pickle.dump(gw, open(gw_pkl_path, "wb"))
+print("Saved vxc cache =", vxc_path, flush=True)
